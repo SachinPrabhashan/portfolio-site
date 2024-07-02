95ed896ae4ee1cd5125b0ec82c5a6d6e72a6aebd
SachinPrabhashan <sachinprabhashan@gmail.com>
2024-07-02 20:11:35 +0530

    Day 01

---
+++ src/components/hero.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import ProfileImg from 
+
+function Hero() {
+
+    return (
+        <div className="herosection">
+            <div>
+                <h1>I'am Sachin</h1>
+            </div>
+            <div>
+                <h3>Web Developer & Frontend Developer</h3>
+            </div>
+
+            <div>
+                <img src="" />
+            </div>
+
+            <div className="aboutsection">
+                <h1>About</h1>
+                <p>
+                    Experienced developer skilled in software and web development, proficient in HTML, CSS and JavaScript. Specializes in creating responsive web apps and robust software systems. Manages projects from conception to deployment, ensuring efficiency and user satisfaction. Detail-oriented problem-solver adept at adapting to new technologies and collaborating within teams to deliver projects on time and within budget.
+                </p>
+            </div>
+
+        </div>
+    )
+
+}
+
+
+
+export default Hero;
+
---
+++ src/components/skills.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Skills() {
+
+    return (
+        <div className="skillsection">
+            <div>
+                <h1>Skills</h1>
+            </div>
+
+        </div>
+    )
+
+}
+
+
+
+export default Skills;
